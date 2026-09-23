c8ec91c39aad4abae6930c7302b44cad98998568
MagnumEntity <dacrecagape01@gmail.com>
2026-09-23 20:45:58 +0800

    Implement daily reset

--- includes/functions.php
+++ includes/functions.php
@@ -14,32 +14,32 @@ function escape_html($string) {
 }
 
 /**
- * Get the start and end datetime for the current tracking day.
- * Returns an array ['start' => 'Y-m-d H:i:s', 'end' => 'Y-m-d H:i:s']
+ * Get the start and next_start datetime for the current tracking day.
+ * Returns an array ['start' => 'Y-m-d H:i:s', 'next_start' => 'Y-m-d H:i:s']
  */
 function get_tracking_day_bounds($reset_time_str) {
-    // Current time
-    $now = new DateTime();
+    $timezone = new DateTimeZone('Asia/Manila');
+    // Current time in Manila timezone
+    $now = new DateTime('now', $timezone);
     
-    // Create a DateTime object for today's reset time
-    $reset_today = new DateTime($now->format('Y-m-d') . ' ' . $reset_time_str);
+    // Create a DateTime object for today's reset time in Manila timezone
+    $reset_today = new DateTime($now->format('Y-m-d') . ' ' . $reset_time_str, $timezone);
     
     if ($now < $reset_today) {
         // If current time is before today's reset time, we are still in yesterday's tracking day
         $start = clone $reset_today;
         $start->modify('-1 day');
-        $end = clone $reset_today;
-        $end->modify('-1 second');
+        $next_start = clone $reset_today;
     } else {
         // We are in today's tracking day
         $start = clone $reset_today;
-        $end = clone $reset_today;
-        $end->modify('+1 day')->modify('-1 second');
+        $next_start = clone $reset_today;
+        $next_start->modify('+1 day');
     }
     
     return [
         'start' => $start->format('Y-m-d H:i:s'),
-        'end' => $end->format('Y-m-d H:i:s')
+        'next_start' => $next_start->format('Y-m-d H:i:s')
     ];
 }
 
@@ -54,9 +54,9 @@ function get_current_tracking_day_intake($pdo, $user_id, $reset_time_str) {
         FROM water_intake_records 
         WHERE user_id = ? 
         AND recorded_at >= ? 
-        AND recorded_at <= ?
+        AND recorded_at < ?
     ");
-    $stmt->execute([$user_id, $bounds['start'], $bounds['end']]);
+    $stmt->execute([$user_id, $bounds['start'], $bounds['next_start']]);
     $row = $stmt->fetch();
     return (int) $row['total'];
 }
